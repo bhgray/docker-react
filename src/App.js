aa4353e044659cb03d67877cfb3c6c3ccd0d43f3
Brent Gray <bhgray@gmail.com>
2019-12-26 22:00:18 -0500

    dumb

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello, world!  Again. and again.
+          I'm changing this stupid text just to see how long it takes.
         </p>
         <a
           className="App-link"
